76a4136c12525033df13067a123256bd1ba00ec2
Benjamin Klarer <benjaminklarer@gmail.com>
2022-07-06 16:40:24 -0700

    GET data from server, organize in object

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
-const BASE_URL = "https://api.coingecko.com/api/v3/coins/";
+const BASE_URL = "https://api.coingecko.com/api/v3/search?query=";
 
 const submitBttn = document.querySelector("#submit-button");
 const tickerInput = document.querySelector("#ticker").value;
+const stockForm = document.querySelector("#stock-form");
 
 document.addEventListener("submit", handleSubmit)
 
@@ -11,13 +12,30 @@ function handleSubmit (e) {
     
     console.log("I work");
     
-    fetch(BASE_URL + "list")
+    
+
+    fetch(BASE_URL + `${tickerInput}`)
         .then(resp => resp.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            searchResults(data)
+            stockForm.reset()
+            }
+        )
         .catch(error => 
         console.log(error))
 
 }
 
 
-
+function searchResults (data) {
+    const results = {}
+    const coin = data.coins
+    for(i=0; i<10; i++) {
+        results[i] = coin[i]
+        // results.search.name = coin[i].name
+        // results.search.symbol = coin[i].symbol
+        // results.search.market_cap_rank = coin[i].market_cap_rank
+    }
+    console.log(results)
+}
